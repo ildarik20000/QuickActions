cadb2e7ecd0dbadf7bf2c6cb9586e0f4034a7a8c
Mazayker <ildar@mazit.ru>
2024-09-19 20:14:59 +0300

    added ios subtitle

--- packages/pigeon/platform_tests/shared_test_plugin_code/lib/generated.dart
+++
@@ -1,7 +0,0 @@
-// Copyright 2013 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-export 'src/generated/core_tests.gen.dart';
-export 'src/generated/proxy_api_tests.gen.dart'
-    show ProxyApiSuperClass, ProxyApiTestClass, ProxyApiTestEnum;
